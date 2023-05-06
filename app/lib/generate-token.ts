d38c35b1cd28bef6efb54012386a0765f2d5055f
Vasco Andrade e Silva <vasco@byclosure.com>
2023-05-06 13:13:37 +0100

    removing the app source code

--- app/lib/generate-token.ts
+++
@@ -1,9 +0,0 @@
-import { sign, Secret, SignOptions } from "jsonwebtoken";
-
-export function generateToken(
-  payload: string | object | Buffer,
-  secret: Secret,
-  options?: SignOptions | undefined
-): string {
-  return sign(payload, secret, options);
-}
